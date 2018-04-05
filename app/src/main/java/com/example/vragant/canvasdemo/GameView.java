40bc0ce9d2c91d4a404487104973353ad8b41297
Edward Vescera <edward.vescera@gmail.com>
2018-04-05 18:52:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vragant/canvasdemo/GameView.java
@@ -0,0 +1,66 @@
+package com.example.vragant.canvasdemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private MainThread thread;
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(),this);
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        thread.setRunning(true);
+        thread.start();
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while (retry) {
+            try {
+                thread.setRunning(false);
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            retry = false;
+        }
+
+    }
+
+    //TODO: hacer una clase por cada elemento del juego
+    //tecnicamente aqui podriamos hacer el juego, pero no seria eficiente
+    @Override
+    public void draw (Canvas canvas) {
+        super.draw(canvas);
+        if (canvas != null) {
+            canvas.drawColor(Color.WHITE);
+            Paint paint = new Paint();
+            paint.setColor(Color.rgb(250,0,0));
+            canvas.drawRect(100,100,200,200, paint);
+
+        }
+    }
+
+    public void update() {
+        //TODO: falta esta mierda
+    }
+}
