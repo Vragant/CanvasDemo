91a0e941a37cde6195421d48ca74b036328140bb
Guillermo Isaac Arvizu Juarez <memoarvizu12@gmail.com>
2018-05-05 18:02:47 -0500

    Malla con cuadrados y lineas

--- app/src/main/java/com/example/vragant/canvasdemo/GameView.java
+++ app/src/main/java/com/example/vragant/canvasdemo/GameView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
@@ -52,11 +53,72 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     public void draw (Canvas canvas) {
         super.draw(canvas);
         if (canvas != null) {
-            canvas.drawColor(Color.WHITE);
-            Paint paint = new Paint();
-            paint.setColor(Color.rgb(250,0,0));
-            canvas.drawRect(100,100,200,200, paint);
 
+            canvas.drawRGB(114,118, 249);
+
+            Paint paint = new Paint();
+
+            //Malla con cuadrados
+            
+            int GRID_WIDTH = 9;
+            int GRID_HEIGHT = 9;
+            int GRID_SIZE = 75;
+
+            for(int i = 0; i < GRID_WIDTH; i++) {
+                for(int j = 0; j < GRID_HEIGHT; j++) {
+                    int left = i * (GRID_SIZE + 5);
+                    int top = j * (GRID_SIZE + 5);
+                    int right = left + GRID_SIZE;
+                    int bottom = top + GRID_SIZE;
+                    paint.setColor(Color.WHITE);
+                    canvas.drawRect(new Rect(left, top, right, bottom), paint);
+                }
+            }
+
+            //Malla con lineas
+
+            /*paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(5);
+
+            float startX;
+            float stopX;
+            float startY;
+            float stopY;
+
+            int width = canvas.getWidth();
+            int height = canvas.getHeight();
+
+            int gridSize = 10;
+            int gridSpacing = Math.min(width, height) / gridSize;
+            int boardSize = gridSize * gridSpacing;
+
+            int xOffset = (width - boardSize)/2;
+            int yOffset = (height - boardSize)/2;
+
+            //Vertical Grid-lines
+            for (int i = 0; i < gridSize; i++) {
+
+                startX = xOffset + i*gridSpacing;
+                startY = yOffset;
+
+                stopX = startX;
+                stopY = startY + boardSize;
+
+                canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+            }
+
+            //Horizontal Grid-lines
+            for (int i = 0; i < gridSize; i++) {
+
+                startX = xOffset;
+                startY = yOffset + i*gridSpacing;
+
+                stopX = startX + boardSize;
+                stopY = startY;
+
+                canvas.drawLine(startX, startY, stopX, stopY, paint);
+            }*/
         }
     }
 
